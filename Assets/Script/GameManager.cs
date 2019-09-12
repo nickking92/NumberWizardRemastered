379b66931577e4b2d4addfe2a580823987f68ceb
Nikola <nickking92@gmail.com>
2019-09-12 16:31:20 +0200

    NW

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    Text TextComponent;
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    int guess;
+    int guessCounter;
+    // Use this for initialization
+    void Start() {
+
+        StartGame();
+    }
+
+
+    void StartGame()
+    {
+
+        NextGuess();
+
+    }
+
+    public void GuessCount()
+    {
+        guessCounter++;
+        if (guessCounter == 4)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+    public void clickedForHigher()
+    {
+
+        if (guess < 1000)
+        {
+            min = guess + 1;
+        }
+        else
+        {
+            min = guess;
+        }
+                NextGuess();
+            
+    }
+    public void clickedForLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        //guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+
+    }
+
+
+}
---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    private string scenes;
+    public void LoadScene()
+    {
+        int currentSceneIndex=SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+    public void LoadStartScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
